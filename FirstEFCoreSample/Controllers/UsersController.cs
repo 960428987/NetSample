a1cedb6fe6e4fe821c3ca46b6513ce49e39f43a4
960428987 <960428987@qq.com>
2021-08-25 17:24:15 +0800

    remove remark

--- FirstEFCoreSample/Controllers/UsersController.cs
+++ FirstEFCoreSample/Controllers/UsersController.cs
@@ -79,8 +79,6 @@ namespace FirstEFCoreSample.Controllers
         }
 
         // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,UserName,Password")] User user)
