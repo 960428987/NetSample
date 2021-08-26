769969a2315bd8274811c2ff79c4dd74bad5a77f
960428987 <960428987@qq.com>
2021-08-26 11:17:59 +0800

    添加hl7协议解析demo

---
+++ HL7Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using HL7.Dotnetcore;
+using System;
+
+namespace HL7Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hl7_str = @"MSH|^~\&|VOXELCLOUD|CENTER1|LIMAGE|CENTER|20210826102604.0525||ACK^T02^ACK|377101|P|2.5
+MSA|AR|377101|No appropriate destination could be found to which this message could be routed";
+            Message message = new Message(hl7_str);
+            bool f = message.ParseMessage();
+            string s = message.GetValue("MSH.9");
+            bool fff = message.SetValue("MSH.9", "ACK");
+            string ss = message.GetValue("MSH.9");
+            //SetValue之后需要SerializeMessage一下
+            string strUpdatedMsg = message.SerializeMessage(false);
+            Console.WriteLine(message.HL7Message);
+            Console.ReadLine();
+        }
+    }
+}
